02d66acf726c8a824719aa830a8bb15055c44c0e
TheMostHigh <tallestlegacy@gmail.com>
2025-06-20 01:09:12 +0300

    fix: mode switcher
    - Update modeswitcher logic
    - swap out lucide-svelte library

--- src/lib/components/layout/ModeSwitcher.svelte
+++ src/lib/components/layout/ModeSwitcher.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import Sun from 'lucide-svelte/icons/sun';
-	import Moon from 'lucide-svelte/icons/moon';
+	import { Sun, Moon } from '@lucide/svelte';
 
-	import { toggleMode } from 'mode-watcher';
+	import { mode, toggleMode } from 'mode-watcher';
 	import { Button } from '@/lib/components/ui/button';
 </script>
 
-<Button on:click={toggleMode} variant="ghost" size="icon">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
+<Button onclick={toggleMode} variant="ghost" size="icon">
+	{#if mode.current === 'light'}
+		<Sun size={32} />
+	{/if}
+	{#if mode.current === 'dark'}
+		<Moon size={32} />
+	{/if}
 	<span class="sr-only">Toggle theme</span>
 </Button>
